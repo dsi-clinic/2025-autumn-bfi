3b1be6d8621ee0124c39b5a0bf12113ed03c601f
Cynthia Zeng <cynthiazeng@MacBook-Air-93.local>
2025-11-17 21:02:06 -0600

    addressed feedbac

--- utils/indiv_msas/get_data.py
+++ utils/indiv_msas/get_data.py
@@ -4,112 +4,123 @@ Includes one function for each supplementary csv
 """
 
 import io
+import logging
 import os
 import zipfile
 from pathlib import Path
 
 import requests
+from requests.exceptions import ReadTimeout, RequestException
 
-DATA_DIR = Path(os.environ["DATA_DIR"])
+RAW_DATA_DIR = Path(os.environ["RAW_DATA_DIR"])
 
-# Make raw data urls hyper-parameters
-RAW_1980_POP_DATA_URL = (
+# Make raw data urls hyper-parameters within dictionaries
+RAW_CENSUS_POP_DATA_URLS = {
     "https://www2.census.gov/programs-surveys/popest/datasets/"
-    "1980-1990/counties/asrh/pe-02.csv"
-)
-
-RAW_2022_POP_DATA_URL = (
+    "1980-1990/counties/asrh/pe-02.csv": "1980",
     "https://www2.census.gov/programs-surveys/popest/datasets/"
-    "2020-2023/metro/asrh/cbsa-est2023-alldata-char.csv"
-)
+    "2020-2023/metro/asrh/cbsa-est2023-alldata-char.csv": "2022",
+}
+
+UBLA_LABOR_DATA_ZIP_URLS_AND_RAW_PATHS = {
+    "https://data.bls.gov/cew/data/files/1980/sic/csv/"
+    "sic_1980_annual_by_industry.zip": "sic.1980.annual.by_industry/"
+    "sic.1980.annual 0Z (All Industries).csv",
+    "https://data.bls.gov/cew/data/files/2022/csv/"
+    "2022_annual_by_industry.zip": "2022.annual.by_industry/"
+    "2022.annual 10 10 Total, all industries.csv",
+}
+
+logger = logging.getLogger(__name__)
 
 
-def get_1980_pop(url: str = RAW_1980_POP_DATA_URL) -> None:
-    """Gets csv containing 1980 population data.
+def get_census_pop(data_urls: dict[str, str] = RAW_CENSUS_POP_DATA_URLS) -> None:
+    """Gets csvs containing 1980 and 2022 population data.
 
     From US Census Bureau.
     """
-    r = requests.get(url, timeout=10)
-    r.raise_for_status()
+    for url, year in data_urls.items():
+        # gets data from URL
+        try:
+            r = requests.get(url, timeout=30)
+            r.raise_for_status()
+        except ReadTimeout as exc:
+            logger.error(f"Timed out while downloading {year} census data from {url}")
+            logger.exception(exc)
+            continue
+        except RequestException as exc:
+            logger.error(f"Failed to download {year} population data from", url)
+            logger.exception(exc)
+            return
 
-    output_file = DATA_DIR / "pop_1980.csv"
+        # saves raw data
+        output_file = RAW_DATA_DIR / f"pop_{year}.csv"
 
-    with output_file.open("wb") as f:
-        f.write(r.content)
+        try:
+            with output_file.open("wb") as f:
+                f.write(r.content)
+        except OSError as exc:
+            logger.error("Failed to write output file: %s", output_file)
+            logger.exception(exc)
+            return
 
-    print("Saved pop_1980.csv to", output_file.resolve())
+        logger.info(f"Saved pop_{year}.csv to", output_file.resolve())
     return
 
 
-def get_2022_pop(url: str = RAW_2022_POP_DATA_URL) -> None:
-    """Gets csv containing 2022 population data.
-
-    From US Census Bureau.
-    """
-    r = requests.get(url, timeout=10)
-    r.raise_for_status()
-
-    output_file = DATA_DIR / "pop_2022.csv"
-
-    with output_file.open("wb") as f:
-        f.write(r.content)
-
-    print("Saved pop_2022.csv to", output_file.resolve())
-    return
-
-
-def get_1980_labor() -> None:
+def get_ubls_labor(
+    zip_urls: dict[str, str] = UBLA_LABOR_DATA_ZIP_URLS_AND_RAW_PATHS,
+) -> None:
     """Unzips folder containing 1980 employment data for all industries.
 
     From US Bureau of Labor Statistics.
     """
-    url = "https://data.bls.gov/cew/data/files/1980/sic/csv/sic_1980_annual_by_industry.zip"
+    for zip_url, path in zip_urls.items():
+        # get year of data
+        if "1980" in zip_url:
+            year = "1980"
+        else:
+            year = "2022"
 
-    r = requests.get(url, timeout=10)
-    r.raise_for_status()
+        # get zipfile containing data
+        try:
+            r = requests.get(zip_url, timeout=30)
+            r.raise_for_status()
+        except RequestException as exc:
+            logger.error(
+                f"Failed to download {year} labor data zipfile from %s", zip_url
+            )
+            logger.exception(exc)
+            return
 
-    output_file = DATA_DIR / "labor_1980.csv"
+        # unzip and save raw data
+        output_file = RAW_DATA_DIR / f"labor_{year}.csv"
 
-    with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-        with (
-            z.open(
-                "sic.1980.annual.by_industry/sic.1980.annual 0Z (All Industries).csv"
-            ) as source,
-            output_file.open("wb") as target,
-        ):
-            target.write(source.read())
+        try:
+            with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+                with (
+                    z.open(path) as source,
+                    output_file.open("wb") as target,
+                ):
+                    target.write(source.read())
 
-    print("Saved labor_1980.csv to", output_file.resolve())
-    return
+        except zipfile.BadZipFile:
+            logger.error("The downloaded file is not a valid ZIP archive.")
+            return
 
+        except KeyError:
+            logger.error("Expected file was NOT found inside the ZIP:\n", path)
+            return
 
-def get_2022_labor() -> None:
-    """Unzips folder containing 2022 employment data for all industries.
+        except OSError as exc:
+            logger.error("Failed to write output file: %s", output_file)
+            logger.exception(exc)
+            return
 
-    From US Bureau of Labor Statistics.
-    """
-    url = "https://data.bls.gov/cew/data/files/2022/csv/2022_annual_by_industry.zip"
-
-    r = requests.get(url, timeout=10)
-    r.raise_for_status()
-
-    output_file = DATA_DIR / "labor_2022.csv"
-
-    with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-        with (
-            z.open(
-                "2022.annual.by_industry/2022.annual 10 10 Total, all industries.csv"
-            ) as source,
-            output_file.open("wb") as target,
-        ):
-            target.write(source.read())
-
-    print("Saved labor_2022.csv to", output_file.resolve())
+        logger.info(f"Saved labor_{year}.csv to", output_file.resolve())
     return
 
 
 if __name__ == "__main__":
-    get_1980_pop()
-    get_2022_pop()
-    get_1980_labor()
-    get_2022_labor()
+    get_census_pop()
+    get_ubls_labor()
